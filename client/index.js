b670bfc643c1d94c0c98f4c052bc4c3f0e1fd43f
Zach Baker <zach.r.baker1@gmail.com>
2017-10-13 11:15:02 -0700

    redux dev tools

--- client/index.js
+++ client/index.js
@@ -4,7 +4,9 @@ import Wrap from './components/Wrap'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import counterApp from './reducers' // i dont know
-let store = createStore(counterApp)
+let store = createStore(counterApp,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 
 render(
 	<Provider store={store} >
